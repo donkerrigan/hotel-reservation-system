85dc26bc7c7365bf303b3dd3533bf51d065e1efb
Don Kerrigan <dak23@students.uwf.edu>
2016-11-26 13:12:50 -0600

    Added a menu to loop on. Search function not working properly but exit option works okay.

--- src/HotelSystem.java
+++ src/HotelSystem.java
@@ -1,20 +1,78 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  * Created by Don Kerrigan on 11/17/2016.
  */
 public class HotelSystem {
     public HotelBrand[] brands;
+    Scanner input;
 
     public HotelSystem()
     {
         brands = new HotelBrand[10];
+        input = new Scanner(System.in);
         for(int i=0; i<10; i++)
             brands[i] = new HotelBrand("Brand " + i);
+        displayMenu();
     }
 
-    public void main(String[] args)
+    public static void main(String[] args)
     {
         HotelSystem system = new HotelSystem();
 
+    }
+
+    private void displayMenu()
+    {
+        int response = 0;
+        while(response != 5)
+        {
+            System.out.println("\nHotel Reservation Main Menu: \n");
+            System.out.println("1: Display Available Hotel Brands.");
+            System.out.println("2: Search for Hotel Brand Name");
+            System.out.println("3: ");
+            System.out.println("4: ");
+            System.out.println("5: Exit System");
+            response = input.nextInt();
+
+            switch(response) {
+                case 1:
+                    break;
+                case 2: searchHotelBrand();
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                default:
+                    System.out.println("Invalid Selection, please try again.");
+                    break;
+            }
+        }
+    }
+
+    public void searchHotelBrand()
+    {
+        String search;
+        List<HotelBrand> results = new ArrayList<HotelBrand>();
+        System.out.println("\nEnter your search for Hotel Brand Names: ");
+        search = input.nextLine();
+        for(int i=0; i<brands.length; i++)
+        {
+            if(brands[i].getHotelBrand() == search)
+                results.add(brands[i]);
+        }
+        if(results.size()==0) {
+            System.out.println("Sorry no results were found.");
+            return;
+        }
+        System.out.println("Showing search results: ");
+        for(int i=0; i<results.size(); i++)
+            System.out.println(i + ": " + results.get(i).getHotelBrand());
 
     }
 }
